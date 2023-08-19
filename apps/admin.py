022d0a758cc38687024a819bdd83d82b8db41960
marufbysl <momenur.islam@byslglobal.com>
2023-08-19 22:54:36 +0600

    apps app added

--- apps/admin.py
+++ apps/admin.py
@@ -1,3 +1,63 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+@admin.register(App)
+class AppAdmin(admin.ModelAdmin):
+
+    list_display=('name','created_by', 'updated_by')
+    readonly_fields = ['created_by','updated_by']
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+
+            obj.created_by = request.user
+        else:
+            # Existing object is being changed, update the updated_by field
+            obj.updated_by = request.user
+            print(request.user)
+        super().save_model(request, obj, form, change)
+# admin.site.register(Blog,BlogAdmin)
+@admin.register(AppFeature)
+class AppFeatureAdmin(admin.ModelAdmin):
+    list_display=['title','created_by','updated_by']
+    readonly_fields = ['created_by','updated_by']
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+
+            obj.created_by = request.user
+        else:
+            # Existing object is being changed, update the updated_by field
+            obj.updated_by = request.user
+            print(request.user)
+        super().save_model(request, obj, form, change)
+
+@admin.register(Pricing)
+class PricingAdmin(admin.ModelAdmin):
+    list_display=['title','created_by','updated_by']
+    readonly_fields = ['created_by','updated_by']
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+
+            obj.created_by = request.user
+        else:
+            # Existing object is being changed, update the updated_by field
+            obj.updated_by = request.user
+            print(request.user)
+        super().save_model(request, obj, form, change)
+
+@admin.register(PlanFeature)
+class PlanFeatureAdmin(admin.ModelAdmin):
+    list_display=['name','created_by','updated_by']
+    readonly_fields = ['created_by','updated_by']
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+
+            obj.created_by = request.user
+        else:
+            # Existing object is being changed, update the updated_by field
+            obj.updated_by = request.user
+            print(request.user)
+        super().save_model(request, obj, form, change)
--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django_quill.fields import QuillField
+ 
 
 # Create your models here.
+from blog.models import BaseModel
+
+
+
+class App(BaseModel):
+    name= models.CharField(max_length=255)
+    description=models.TextField()
+    icon=models.ImageField( upload_to='static\images', height_field=None, width_field=None, max_length=None)
+    is_active=models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+class AppFeature(BaseModel):
+    app_id = models.ForeignKey("apps.App", verbose_name=("ID"), on_delete=models.CASCADE)
+    title = models.CharField(max_length=1000)
+    short_description = models.TextField()
+    content = QuillField()
+    icon = models.ImageField(upload_to='static\images', height_field=None, width_field=None, max_length=None)
+    is_active =models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+    
+class Pricing(BaseModel):
+    title= models.CharField(max_length=250)
+    short_description=models.TextField()
+    monthly_bill=models.FloatField()
+    annual_bill=models.FloatField()
+
+    def __str__(self):
+        return self.title
+
+class PlanFeature(BaseModel):
+    name= models.CharField( max_length=250)
+    description =models.TextField()
+
+    def __str__(self):
+        return self.name
